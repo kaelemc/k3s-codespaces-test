1c9082ff687844e0da2b62a572ba42d1e0d55c6e
Kaelem Chandra <kc@kaelem.net>
2026-01-04 08:26:26 +1300

    Delete tar after untarred

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,4 +1,5 @@
 cd /
-docker run --rm -v $(pwd):/workspace ghcr.io/oras-project/oras:v1.3.0 pull ghcr.io/kaelemc/k3s-zst
+docker run --rm -v $(pwd):/workspace ghcr.io/oras-project/oras:v1.3.0 pull ghcr.io/kaelemc/k3s-zst:latest
 tar -xf k3s.tar.zst
+rm -f k3s.tar.zst
 ls -lah
