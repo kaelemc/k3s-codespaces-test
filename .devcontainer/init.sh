e5d269b7bc7789dc90cbe844e70647c2b6d3d0d0
Kaelem Chandra <kc@kaelem.net>
2026-01-04 08:23:32 +1300

    pin oras

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,4 +1,4 @@
 cd /
-docker run --rm -v $(pwd):/workspace ghcr.io/oras-project/oras pull ghcr.io/kaelemc/k3s-zst
-tar -xvf k3s.tar.zst
+docker run --rm -v $(pwd):/workspace ghcr.io/oras-project/oras:v1.3.0 pull ghcr.io/kaelemc/k3s-zst
+tar -xf k3s.tar.zst
 ls -lah
