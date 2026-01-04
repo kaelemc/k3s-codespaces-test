c6b5b412669634eca82ad88256ed741b0fed5aff
Kaelem Chandra <kc@kaelem.net>
2026-01-05 06:40:51 +1300

    tweak script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,5 @@
+k3s-killall.sh
 tar --exclude='*/io.containerd.snapshotter.v1.overlayfs/*' -xf /k3s.tar.zst -C /var/lib/rancher/k3s --strip-components=1
 rm -f /k3s.tar.zst
+systemctl start k3s
 ls -lah /var/lib/rancher/k3s
