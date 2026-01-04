a2d4bc824ff472374f58a0ea7950e89f034acfcd
Kaelem Chandra <kc@kaelem.net>
2026-01-05 03:29:22 +1300

    Use fat image

--- .devcontainer/init.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p /k3s-data
-# cd /
-# docker run --rm -v $(pwd):/workspace ghcr.io/oras-project/oras:v1.3.0 pull ghcr.io/kaelemc/k3s-zst:latest
-# tar -xf k3s.tar.zst
-# rm -f k3s.tar.zst
-# ls -lah
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -2,22 +2,18 @@
 set -e
 
 mount --make-rshared /
+
 ln -sf /host-docker.sock /var/run/docker.sock
 
-systemctl stop k3s
-rm -rf /var/lib/rancher/k3s/*
-tar -xf /k3s.tar.zst -C /var/lib/rancher/k3s --strip-components=1
-rm -f /k3s.tar.zst
+# for cg in $(find /sys/fs/cgroup/kubepods.slice -type d -name "cri-containerd-*" 2>/dev/null | sort -r); do
+#   rmdir "$cg" 2>/dev/null || true
+# done
 
-for cg in $(find /sys/fs/cgroup/kubepods.slice -type d -name "cri-containerd-*" 2>/dev/null | sort -r); do
-  rmdir "$cg" 2>/dev/null || true
-done
+# systemctl restart k3s
 
-systemctl start k3s
+# until kubectl get nodes &>/dev/null; do
+#   sleep 1
+# done
+# sleep 1
 
-until kubectl get nodes &>/dev/null; do
-  sleep 1
-done
-sleep 1
-
-kubectl delete --all pods -n eda-system --force
+# kubectl delete --all pods -n eda-system --force
